903ae91f5e6feeae810ee70106ca1ebc50e934ac
DrWarpMan <drwarpman@gmail.com>
2024-07-21 10:41:36 +0200

    restaurant json response for index + show

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -2,25 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\RestaurantResource;
 use App\Models\Restaurant;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
 
 class RestaurantController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): JsonResource
     {
-        //
+        return RestaurantResource::collection(Restaurant::paginate());
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Restaurant $restaurant)
+    public function show(Restaurant $restaurant): JsonResource
     {
-        //
+        return new RestaurantResource($restaurant);
     }
 
     /**
