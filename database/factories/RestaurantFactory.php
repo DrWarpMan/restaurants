4e24bc3cc29b4a74bbecbe9c14df094faddb181b
DrWarpMan <drwarpman@gmail.com>
2024-07-21 10:41:08 +0200

    restaurant seeder+factory

---
+++ database/factories/RestaurantFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Restaurant;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Restaurant>
+ */
+class RestaurantFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var class-string<\Illuminate\Database\Eloquent\Model>
+     */
+    protected $model = Restaurant::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->company(),
+            'restaurant_id' => fake()->unique()->regexify('[a-z]{5}'),
+            'cuisine' => fake()->word(),
+            'price' => fake()->numberBetween(1, 5),
+            'rating' => fake()->numberBetween(1, 5),
+            'location' => fake()->address(),
+            'description' => fake()->optional(0.5, null)->sentence(),
+        ];
+    }
+}
