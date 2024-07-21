65a717b565832cf2a1b169b03913beb582f48786
DrWarpMan <drwarpman@gmail.com>
2024-07-21 10:11:31 +0200

    prepare restaurant model, controller, migration

---
+++ app/Http/Controllers/RestaurantController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Restaurant;
+use Illuminate\Http\Request;
+
+class RestaurantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Restaurant $restaurant)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_07_21_080907_create_restaurants_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('restaurant_id')->unique();
+            $table->string('cuisine')->nullable();
+            $table->tinyInteger('price')->nullable();
+            $table->tinyInteger('rating')->nullable();
+            $table->string('location')->nullable();
+            $table->text('description')->nullable();
+            // TODO: business hours
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('restaurants');
+    }
+};
