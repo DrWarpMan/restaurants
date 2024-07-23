7dfe77fbc04b16a04222be5b38d3172d2da1f308
DrWarpMan <drwarpman@gmail.com>
2024-07-23 20:19:00 +0200

    docs

--- database/seeders/RestaurantSeeder.php
+++ database/seeders/RestaurantSeeder.php
@@ -31,8 +31,8 @@ class RestaurantSeeder extends Seeder
                     ->create([
                         "restaurant_id" => $restaurant->id,
                         "day" => $day,
-                        "opens" => fake()->numberBetween(0, 86400 / 2), // 0-12:00
-                        "closes" => fake()->numberBetween(86400 / 2 + 1, 86400), // 12:01-24:00
+                        "opens" => fake()->numberBetween(0, 86400 / 2), // 0:00:00 - 12:00:00
+                        "closes" => fake()->numberBetween(86400 / 2 + 1, 86400), // 12:00:01 - 24:00:00
                     ]);
             }
         });
